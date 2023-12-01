94a9884dcdec347aec0194d7e80346e705c50bf6
GeoJulien <dev@ingeoveritas.com>
2023-12-01 17:04:14 +0100

    MAJ style de Noël

--- prettymaps/prettymaps_04_style_noel.py
+++ prettymaps/prettymaps_04_style_noel.py
@@ -12,14 +12,15 @@ with preset_christmas_file.open(mode="r", encoding="UTF-8") as file:
 # enregistre le préréglage
 prettymaps.create_preset("christmas", **christmas_preset)
 
-# Define the location - Rovaniemi, Finland
-location = "Joulupukin Pajakylä, Rovaniemi, Finland"
-location = (66.5070, 25.7312)
+# Define the location - Rovaniemi, Finland = village du Père Noël
+# location = "Joulupukin Pajakylä, Rovaniemi, Finland"
+# location = (66.5070, 25.7312)
+location = (48.646201, -2.015234)  # bassin Jacques Cartier, Saint-Malo, France
 
 # Plot the map using the 'christmas' preset
 plot = prettymaps.plot(
     query=location,
     preset="christmas",  # Use the Christmas preset
     save_as="test_noel.png",
-    figsize=(10, 10),  # Define the size of the plot
+    # figsize=(10, 10),  # Define the size of the plot
 )
